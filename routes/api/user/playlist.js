ca556d4530be972f68ad6e096bf742f01f529f35
qxiang <hsiangpet@gmail.com>
2021-04-14 22:07:23 -0400

    Add user playlist tracks endpoint

--- routes/api/user/playlist.js
+++ routes/api/user/playlist.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import userModel from '../../../models/user.js';
+import tracksModel from '../../../models/tracks.js';
 
 const {listSpotifyPlaylists} = userModel;
+const {getSpotifyPlaylistTracks} = tracksModel;
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -10,6 +12,12 @@ router.get('/', function(req, res) {
   listSpotifyPlaylists(req.session.tokens.accessToken)
       .then((data) => res.json(data))
       .catch((err) => res.json(err));
+});
+
+router.get('/:id/tracks', function(req, res) {
+  getSpotifyPlaylistTracks(req.params.id, req.session.tokens.accessToken)
+      .then((data) => res.json(data))
+      .catch((err) => req.json(err));
 });
 
 
